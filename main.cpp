ad817974eebdc36a8dd14f68856c98715e4d63f1
brianxautumn <shred@brianautumn.com>
2016-03-06 17:06:57 -0800

    I'm able to make a 3d image but it is only showing the sides of the image. Not sure how to visualize the inside

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@
 #include <vtkImageMapper.h>
 #include <vtkImageSliceMapper.h>
 #include <vtkImageSlice.h>
+#include <vtkVolumeProperty.h>
+#include <vtkSmartVolumeMapper.h>
 
 #include <iostream>
 
@@ -35,19 +37,59 @@ int main() {
 
     vtkSmartPointer<vtkImageSliceMapper> imageSliceMapper = vtkSmartPointer<vtkImageSliceMapper>::New();
     imageSliceMapper->SetInputConnection(reader->GetOutputPort());
-    imageSliceMapper->Update();
+    //imageSliceMapper->SetInputData(reader->GetOutput());
+
+    vtkSmartPointer<vtkImageSlice> imageSlice = vtkSmartPointer<vtkImageSlice>::New();
+    imageSlice->SetMapper(imageSliceMapper);
 
 
-    // Visualize
-    vtkSmartPointer<vtkImageViewer2> imageViewer = vtkSmartPointer<vtkImageViewer2>::New();
-    imageViewer->SetInputConnection(reader->GetOutputPort());
-    vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
-    imageViewer->SetupInteractor(renderWindowInteractor);
-    imageViewer->Render();
-    imageViewer->GetRenderer()->ResetCamera();
-    imageViewer->Render();
+    vtkSmartPointer<vtkSmartVolumeMapper> volumeMapper =
+            vtkSmartPointer<vtkSmartVolumeMapper>::New();
+    volumeMapper->SetBlendModeToComposite(); // composite first
+    volumeMapper->SetInputConnection(reader->GetOutputPort());
+
+    vtkSmartPointer<vtkVolumeProperty> volumeProperty =
+            vtkSmartPointer<vtkVolumeProperty>::New();
+    volumeProperty->ShadeOff();
+    volumeProperty->SetInterpolationType(VTK_LINEAR_INTERPOLATION);
+
+
+
+    // Setup renderers
+    vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
+    renderer->AddViewProp(imageSlice);
+    renderer->ResetCamera();
+    renderer->SetBackground(0.1,0.4,0.2);
+
+    // Setup render window
+    vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+    renderWindow->SetSize(500, 500);
+    renderWindow->AddRenderer(renderer);
+
+    // Setup render window interactor
+    vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+            vtkSmartPointer<vtkRenderWindowInteractor>::New();
+
+
+
+
+
+
+    vtkSmartPointer<vtkVolume> volume =
+            vtkSmartPointer<vtkVolume>::New();
+    volume->SetMapper(volumeMapper);
+    volume->SetProperty(volumeProperty);
+    renderer->AddViewProp(volume);
+    renderer->ResetCamera();
+    volumeMapper->SetRequestedRenderModeToRayCast();
+    renderWindow->Render();
+
+    // Render and start interaction
+    renderWindowInteractor->SetRenderWindow(renderWindow);
+    renderWindowInteractor->Initialize();
 
     renderWindowInteractor->Start();
+
 
     return 0;
 }
